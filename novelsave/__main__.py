8c6b1f2da14298c7840c63afb2f261a97cffc354
mHaisham <mHaisham@users.noreply.github.com>
2020-06-29 08:26:59 +0500

    Small Fixes

--- novelsave/__main__.py
+++ novelsave/__main__.py
@@ -2,22 +2,27 @@ import argparse
 from novelsave import NovelSave
 
 parser = argparse.ArgumentParser(description='tool to convert webnovel to epub')
-parser.add_argument('novel', help='either id or url or novel')
-parser.add_argument('-u', '--update', type=bool, action='store_true', help='update webnovel details')
-parser.add_argument('-p', '--pending', type=bool, action='store_true', help='download pending')
-parser.add_argument('-c', '--create', type=bool, action='store_true', help='create epub')
+parser.add_argument('novel', type=str, help='either id or url of novel')
+parser.add_argument('--email', type=str,  help='email credential')
+parser.add_argument('--pass', type=str, help='password credential', dest='password')
+parser.add_argument('-u', '--update', action='store_true', help='update webnovel details')
+parser.add_argument('-p', '--pending', action='store_true', help='download pending')
+parser.add_argument('-c', '--create', action='store_true', help='create epub')
 
 args = parser.parse_args()
 
-# novel id
+# parse novel id
 if 'https' in args.novel:
     novel_id = int(args.novel.split('/')[4])
 else:
     novel_id = int(args.novel)
 
 novelsave = NovelSave(novel_id)
+novelsave.email = args.email
+novelsave.password = args.password
 
-input()
+if not any([args.update, args.pending, args.create]):
+    print('No actions selected')
 
 if args.update:
     novelsave.update_data()

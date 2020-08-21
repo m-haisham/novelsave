f48dd8197bbabb85d7a10a7bfff2e39df81fe7b2
mHaisham <mHaisham@users.noreply.github.com>
2020-08-21 06:59:47 +0500

    Changed task done flag to be calculated everytime

--- novelsave/concurrent/controller.py
+++ novelsave/concurrent/controller.py
@@ -9,10 +9,6 @@ class ConcurrentActionsController:
         self.queue_in = Queue()
         self.queue_out = Queue()
 
-        # used to tell when all tasks done
-        self.remaining_count = AtomicInt(0)
-        self.backlog = 0
-
         self.threads = [
             ActionThread(
                 task,
@@ -37,8 +33,6 @@ class ConcurrentActionsController:
         """
         start the tasks
         """
-        self.remaining_count.set(self.queue_in.qsize())
-
         for thread in self.threads:
             thread.start()
 
@@ -47,15 +41,10 @@ class ConcurrentActionsController:
         """
         :return: whether all the tasks are done
         """
-        if self.backlog > 0:
-            self.backlog -= 1
-            return False
-
         with self.queue_in.all_tasks_done:
             q_in = self.queue_in.unfinished_tasks == 0
 
         with self.queue_out.all_tasks_done:
-            self.backlog = self.queue_out.unfinished_tasks
-            q_out = self.backlog == 0
+            q_out = self.queue_out.unfinished_tasks == 0
 
         return q_in and q_out

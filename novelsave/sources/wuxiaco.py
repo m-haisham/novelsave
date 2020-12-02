83a5c60e9e06f94dbab207c6c65890d64c31ad6f
mHaisham <mHaisham@users.noreply.github.com>
2020-12-02 05:13:15 +0500

    Fixes wuxiaco.py
    greyed out chapters no longer downloaded
    Removed chapter title parsing as Chapter::no is not longer useful

--- novelsave/sources/wuxiaco.py
+++ novelsave/sources/wuxiaco.py
@@ -26,11 +26,15 @@ class WuxiaCo(Source):
 
         chapters = []
         for i, item in enumerate(soup.find_all('a', {'class': 'chapter-item'})):
-            no, title = self._parse_title(item.find('p').text)
+            # wuxiaco uses inline styling(color) to show that chapter isn't ready yet
+            # no need to download chapters without actual content
+            if 'style' in item.attrs.keys():
+                continue
+
+            title = item.find('p').text
 
             chapter = Chapter(
                 index=i,
-                no=int(no),
                 title=title,
                 url=f"{self.base}{item['href']}"
             )
@@ -42,7 +46,7 @@ class WuxiaCo(Source):
     def chapter(self, url: str) -> Chapter:
         soup = self.soup(url)
 
-        no, title = self._parse_title(soup.find('h1', {'class': 'chapter-title'}).text)
+        title = soup.find('h1', {'class': 'chapter-title'}).text
 
         # remove google ads
         for element in soup.find_all('ins', {'class': 'adsbygoogle'}):
@@ -59,33 +63,10 @@ class WuxiaCo(Source):
         content = self._clean_content(content)
 
         return Chapter(
-            no=int(no),
             title=title,
             paragraphs=content,
             url=url
         )
-
-    def _parse_title(self, u_title: str) -> Tuple[int, str]:
-        # when title is affixed by chapter
-        if u_title[:8].lower() == 'chapter ':
-            _, no, title = u_title.split(' ', maxsplit=2)
-
-            try:
-                # Chapter {no} {title}
-                return int(no), title
-            except ValueError:
-                # semi colon after no
-                # Chapter {no}: {title}
-                return int(no[:-1]), title
-
-        try:
-            # {no} {title}
-            no, title = u_title.split(' ', maxsplit=1)
-            return int(no), title
-        except ValueError:
-            # typically volume 0 chapters that doesnt have chapter no.
-            # {title}
-            return -1, u_title
 
     def _clean_content(self, content):
         paragraphs = []

4666b4b9d0b5cabff41939cd96806bd445984d08
mHaisham <mHaisham@users.noreply.github.com>
2021-01-05 01:01:01 +0500

    Added helper methods

--- novelsave/__init__.py
+++ novelsave/__init__.py
@@ -1,4 +1,4 @@
 from .epub import NovelEpub
 from .novelsave import NovelSave
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
--- novelsave/database/tables/decoupled.py
+++ novelsave/database/tables/decoupled.py
@@ -1,16 +1,41 @@
+from pathlib import Path
 from typing import List, TypeVar
 
 from tinydb import TinyDB
 
 from .multi import MultiClassTable
+from ..accessors import IAccessor
+from ...tools import unzip_arguments
 
 T = TypeVar('T')
+
+
+class Decoupled:
+    """
+    moves the data to a new database file
+    database variant to be applied for when the data is highly variable
+    """
+
+    def __init__(self, path: Path, table: IAccessor):
+        pass
+
+    def __new__(cls, *args, **kwargs) -> IAccessor:
+        path, table = unzip_arguments(args, kwargs, ((0, 'path'), (1, 'table')))
+
+        # create a new database
+        db = TinyDB(path / Path(f'{table.table_name}.db'))
+
+        # get table from database
+        table._table = db.table(table.table_name)
+
+        return table
 
 
 class MultiClassDecoupledTable(MultiClassTable):
     """
     MultiClass database variant to be used for when the data is highly variable
     """
+
     def __init__(self, db, path, table: str, cls: T, fields: List[str], identifier: str):
         super(MultiClassDecoupledTable, self).__init__(db, table, cls, fields, identifier)
 

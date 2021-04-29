bbe65852b285efac7adab2d3024d63d18b91d7a0
mHaisham <47662901+mHaisham@users.noreply.github.com>
2021-04-30 00:17:48 +0500

    added custom error messages to config: dir

--- novelsave/cli/config.py
+++ novelsave/cli/config.py
@@ -1,3 +1,4 @@
+import errno
 from pathlib import Path
 
 from ..database import UserConfig
@@ -14,8 +15,22 @@ class CliConfig:
         config = CliConfig(args.verbose)
 
         # updating storage directory
-        if args.dir:
-            config.set_dir(args.dir)
+        try:
+            if args.dir:
+                config.set_dir(args.dir)
+        except IOError as e:
+            if e.errno == 22:  # invalid filename
+                config.console.error(f'The provided path is syntactically incorrect: ({args.dir})')
+            else:
+                config.console.error(str(e))
+            return
+        except ValueError as e:  # check for validation failures
+            config.console.error('''
+Path validation failed. make sure that:
+  - The path exists
+  - The path points a directory
+    ''')
+            return
 
         if args.toggle_banner:
             config.toggle_banner()
@@ -35,11 +50,8 @@ class CliConfig:
             # could throw an OSError: illegal directory names
             dir = Path(_dir).resolve().absolute()
 
-            try:
-                self.user.directory.put(str(dir))
-                line.end(f'"{self.user.directory.get()}", done.')
-            except ValueError as e:  # check for validation failures
-                self.console.error(e)
+            self.user.directory.put(str(dir))
+            line.end(f'"{self.user.directory.get()}", done.')
 
     def toggle_banner(self):
         new_mode = not self.user.show_banner.get()

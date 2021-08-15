9cb1d39fbee478c4a652ef5a8e35c0bb47f0e076
mHaisham <47662901+mHaisham@users.noreply.github.com>
2021-08-15 16:33:51 +0500

    add limit and fix command refresh

--- novelsave/cli/groups/_refresh.py
+++ novelsave/cli/groups/_refresh.py
@@ -6,9 +6,11 @@ from ..main import cli
 
 @cli.command(name='refresh')
 @click.argument('id_or_url')
+@click.option('--limit', type=int, help='Maximum count of chapters to update. Set to 0 or less to skip.')
 def _refresh(
         id_or_url: str,
+        limit: int,
 ):
     """run update and compile consecutively"""
-    controllers.update(id_or_url)
+    controllers.update(id_or_url, limit)
     controllers.compile(id_or_url)

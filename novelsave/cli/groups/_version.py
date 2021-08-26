ee5042f36f1c24c49f3f6bede255e6325a904e93
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-08-26 12:40:28 +0500

    changed version command to print

--- novelsave/cli/groups/_version.py
+++ novelsave/cli/groups/_version.py
@@ -7,4 +7,8 @@ from ... import __version__
 @cli.command(name='version')
 def _version():
     """Show application version"""
-    logger.info(__version__)
+
+    # Using print here instead of logger because, we want the version
+    # as plain possible, also there is no point in printing this
+    # to a log file
+    print(__version__)

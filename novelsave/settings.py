c17291cbfb92c34290aab61e6c46d6d423524e47
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-11-13 22:53:52 +0500

    Modified settings to use fs with support for in-memory files and database
    Signed-off-by: Mensch272 <47662901+mensch272@users.noreply.github.com>

--- novelsave/settings.py
+++ novelsave/settings.py
@@ -1,12 +1,11 @@
 import mimetypes
 import os
-
-import dotenv
 from pathlib import Path
 
+import dotenv
+import fs
 from appdirs import user_config_dir
 from tqdm import tqdm
-
 
 NAME = "novelsave"
 AUTHOR = "Mensch272"
@@ -15,27 +14,37 @@ AUTHOR = "Mensch272"
 dotenv.load_dotenv()  # take environment variables from .env
 
 STORAGE = os.environ.get("STORAGE") or "filesystem"
+LOGTOFILE = bool(int(os.environ.get("LOGTOFILE") or "1"))
+
+fs_storage = {
+    "filesystem": "osfs",
+    "memory": "mem",
+}
 
 # base project directory
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = fs.open_fs(
+    fs_storage[STORAGE] + "://" + str(Path(__file__).resolve().parent.parent)
+)
 
-STATIC_DIR = BASE_DIR / "novelsave/resources"
+STATIC_DIR = "novelsave/resources"
 
 # operating system specific configuration file
 # config directory is used to place logs, config, cache
-CONFIG_DIR = Path(user_config_dir(NAME, AUTHOR))
-CONFIG_DIR.mkdir(parents=True, exist_ok=True)
-CONFIG_FILE = CONFIG_DIR / "config.json"
+CONFIG_PATH = Path(user_config_dir(NAME, AUTHOR))
+CONFIG_DIR = fs.open_fs(fs_storage[STORAGE] + "://" + str(CONFIG_PATH))
+CONFIG_FILE = "config.json"
 
-DATA_DIR = CONFIG_DIR / "data"
-DATA_DIR.mkdir(parents=True, exist_ok=True)
+DATA_DIR = fs.open_fs(fs_storage[STORAGE] + "://" + str(CONFIG_PATH / "data"))
 
-DATABASE_FILE = (CONFIG_DIR / "data.sqlite").resolve()
-DATABASE_URL = "sqlite:///" + str(DATABASE_FILE)
+if STORAGE == "memory":
+    DATABASE_URL = "sqlite:///:memory:"
+else:
+    DATABASE_FILE = (CONFIG_PATH / "data.sqlite").resolve()
+    DATABASE_URL = "sqlite:///" + str(DATABASE_FILE)
 
 # default novel directory, where packaged files such
 # as epub and pdf are stored.
-NOVEL_DIR = Path.home() / "novels"
+NOVEL_DIR = fs.open_fs(fs_storage[STORAGE] + "://" + str(Path.home()))
 
 # the following map defines how files are stored
 # by further subdivision into sub-folders
@@ -61,15 +70,19 @@ LOGGER_CONFIG = {
             "backtrace": False,
             "diagnose": False,
         },
+    ],
+}
+
+if LOGTOFILE:
+    LOGGER_CONFIG["handlers"] += [
         {
-            "sink": CONFIG_DIR / "logs" / "{time}.log",
+            "sink": CONFIG_PATH / "logs" / "{time}.log",
             "level": "TRACE",
             "retention": "2 days",
             "compression": "zip",
             "encoding": "utf-8",
-        },
-    ],
-}
+        }
+    ]
 
 TQDM_CONFIG = {"ncols": 80, "bar_format": "{percentage:3.0f}% |{bar}{r_bar}"}
 

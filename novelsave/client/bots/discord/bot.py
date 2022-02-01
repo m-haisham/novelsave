757971a20f87228d128c28ad3b3a8f9ace554eef
Mensch272 <47662901+mensch272@users.noreply.github.com>
2022-02-02 02:10:13 +0500

    Dev: development setup for discord slash commands

--- novelsave/client/bots/discord/bot.py
+++ novelsave/client/bots/discord/bot.py
@@ -1,7 +1,10 @@
+import functools
+import os
 import sys
 
 from loguru import logger
 
+from novelsave.utils.helpers import dotenv_helper
 from . import config
 
 logger.configure(**config.logger_config())
@@ -14,3 +17,9 @@ except ImportError as e:
 
 
 bot = commands.Bot("$")
+
+dotenv_helper.load_dotenv()
+if os.getenv("MODE", "dev").lower():
+    bot.slash_command = functools.partial(
+        bot.slash_command, guild_ids=[int(os.getenv("GUILD"))]
+    )
